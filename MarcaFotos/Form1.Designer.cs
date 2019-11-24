144aabff8d520272809a8bacfcbf4d00106f4b37
Artinf0 <arthur_henrique01@yahoo.com.br>
2019-11-24 02:48:42 -0300

    Inicial

---
+++ MarcaFotos/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MarcaFotos {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txbFile = new System.Windows.Forms.TextBox();
+            this.txbFolder = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFotos = new System.Windows.Forms.Button();
+            this.btnWaterMark = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txbFile
+            // 
+            this.txbFile.Enabled = false;
+            this.txbFile.Location = new System.Drawing.Point(6, 102);
+            this.txbFile.Name = "txbFile";
+            this.txbFile.Size = new System.Drawing.Size(213, 20);
+            this.txbFile.TabIndex = 1;
+            // 
+            // txbFolder
+            // 
+            this.txbFolder.Enabled = false;
+            this.txbFolder.Location = new System.Drawing.Point(6, 47);
+            this.txbFolder.Name = "txbFolder";
+            this.txbFolder.Size = new System.Drawing.Size(213, 20);
+            this.txbFolder.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFotos);
+            this.groupBox1.Controls.Add(this.btnWaterMark);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txbFolder);
+            this.groupBox1.Controls.Add(this.txbFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 139);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Indique os arquivos";
+            // 
+            // btnFotos
+            // 
+            this.btnFotos.Location = new System.Drawing.Point(225, 45);
+            this.btnFotos.Name = "btnFotos";
+            this.btnFotos.Size = new System.Drawing.Size(28, 23);
+            this.btnFotos.TabIndex = 0;
+            this.btnFotos.Text = "...";
+            this.btnFotos.UseVisualStyleBackColor = true;
+            this.btnFotos.Click += new System.EventHandler(this.btnFotos_Click);
+            // 
+            // btnWaterMark
+            // 
+            this.btnWaterMark.Location = new System.Drawing.Point(225, 102);
+            this.btnWaterMark.Name = "btnWaterMark";
+            this.btnWaterMark.Size = new System.Drawing.Size(28, 23);
+            this.btnWaterMark.TabIndex = 1;
+            this.btnWaterMark.Text = "...";
+            this.btnWaterMark.UseVisualStyleBackColor = true;
+            this.btnWaterMark.Click += new System.EventHandler(this.btnWaterMark_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Arquivo de marca d\'agua";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pasta das fotos";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 198);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 6F);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(9, 22);
+            this.toolStripLabel1.Text = "₢";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 223);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MarcaFoto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txbFile;
+        private System.Windows.Forms.TextBox txbFolder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFotos;
+        private System.Windows.Forms.Button btnWaterMark;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MarcaFotos/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarcaFotos
+{
+    public partial class Form1 : Form
+    {
+
+        string dirFolder = string.Empty;
+        string dirFile = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripLabel1.Text = "Desenvolvido por Artinfo " + DateTime.Now.Year;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(dirFile) && !string.IsNullOrEmpty(dirFolder))
+                try {
+                    AddWaterMark();
+
+                } catch (Exception ex) {
+                    MessageBox.Show(this, "Algo deu errado \n Detalhes: " + ex.Message, "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else
+                MessageBox.Show(this, "Selecione primeiro a pasta das imagens e em seguida a marca d'agua.", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void AddWaterMark()
+        {
+            DirectoryInfo d = new DirectoryInfo(dirFolder);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.JPG"); //Getting Text files
+
+            foreach (FileInfo file in Files) {
+
+                using (Image image = Image.FromFile(file.FullName))
+                using (Image watermarkImage = Image.FromFile(dirFile))
+                using (Graphics imageGraphics = Graphics.FromImage(image)) {
+
+                    int x1 = watermarkImage.Width * 2;
+                    int y1 = watermarkImage.Height * 2;
+
+                    int x = ((image.Width - x1));
+                    int y = ((image.Height - y1));
+
+                    imageGraphics.DrawImage(watermarkImage, x, y, x1, y1);
+
+                    string local = Path.Combine(dirFolder, "mod" + file.Name);
+                    image.Save(local);
+                }
+            }
+            MessageBox.Show(this, "Finalizado!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        #region Eventos button
+        private void btnFotos_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            this.txbFolder.Text = dirFolder = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private void btnWaterMark_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "*.png | *.png";
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.ShowDialog();
+            this.txbFile.Text = dirFile = openFileDialog1.FileName;
+        }
+
+        #endregion
+    }
+}
